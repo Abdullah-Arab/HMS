955820cb927562f8a29b078893136d56c67e0a17
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-04 22:52:11 +0200

    Add database migrations for rooms, reservations, and reservations_rooms tables

---
+++ backend/src/db/migrations/20250104203412_rooms.ts
@@ -0,0 +1,22 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    knex.schema.createTable(
+        "rooms",
+        (table)=>{
+            table.increments("id").primary().unique();
+            table.integer("room_number").notNullable().unique();
+            table.text("name");
+            table.integer("capacity").notNullable();
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+        }
+    );
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    knex.schema.dropTableIfExists("rooms");
+}
+
---
+++ backend/src/db/migrations/20250104203441_reservations_rooms.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("reservations_rooms", (table) => {
+    table.increments("id").primary().unique();
+    table
+      .integer("reservation_id")
+      .notNullable()
+      .references("id")
+      .inTable("reservations");
+    table.integer("room_id").notNullable().references("id").inTable("rooms");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTableIfExists("reservations_rooms");
+}
