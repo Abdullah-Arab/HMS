d388c2d6d0c1e2f0b88f3b13235cb1b8eea6451d
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-06 23:10:32 +0200

    Add TODOs for enhancing reservations service functionality and validation

--- backend/src/services/reservationsService.ts
+++ backend/src/services/reservationsService.ts
@@ -1,6 +1,15 @@
 import reservationsModel from "../models/reservationsModel";
 import { Reservation } from "../types/Reservation";
 
+
+// todo: retrieve upcoming/past reservations for specific guest/room sorted by most recent
+// todo: total amount of upcoming reservations for specific guest/room
+// todo: current reservation for specific guest/room
+// todo: when creating a reservation, check if room is available
+// todo: when creating a reservation, validate check-in and check-out dates
+// todo: when creating a reservation, handle multiple rooms ( in reservation model as well )
+// todo: handle race conditions
+// todo: cancel reservation
 class ReservationsService {
   getAllReservationss = async (page: number = 1, limit: number = 10) => {
     const offset = (page - 1) * limit;
