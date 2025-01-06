1de0ff7fba4932c9bcf31bfd261efbd5bd2fa478
Abdullah Arab <abdo.arab2014@gmail.com>
2025-01-06 15:59:55 +0200

    Refactor request validation middleware to support dynamic source selection and improve error response structure

--- backend/src/middleware/validateRequest.ts
+++ backend/src/middleware/validateRequest.ts
@@ -1,27 +1,25 @@
 import { Value } from "@sinclair/typebox/value";
-import { ValueError } from "@sinclair/typebox/errors";
 import { Request, Response, NextFunction } from "express";
-import { TSchema } from "@sinclair/typebox";
-import { TypeCompiler } from "@sinclair/typebox/compiler";
 
-// Middleware to validate the input
-export const validate = (schema: TSchema) => {
-  const check = TypeCompiler.Compile(schema);
+export function validate(
+  schema: any,
+  source: "body" | "params" | "query" = "body"
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const dataToValidate = req[source];
+    const isValid = Value.Check(schema, dataToValidate);
 
-  return (req: Request, res: Response, next: NextFunction): void => {
-    const result = check.Check(req.body);
-    if (!result) {
-      const errors = [...Value.Errors(schema, req.body)];
-
-      res.status(400).send({
-        error: "Invalid request body",
+    if (!isValid) {
+      const errors = [...Value.Errors(schema, dataToValidate)];
+       res.status(400).json({
+        error: "Invalid request",
         details: errors.map((err) => ({
           path: err.path,
           message: err.message,
         })),
       });
-    } else {
-      next();
     }
+
+    next();
   };
-};
+}
--- backend/src/routes/v1/guestRoutes.ts
+++ backend/src/routes/v1/guestRoutes.ts
@@ -15,9 +15,21 @@ router.get("/", validate(GetGuestsQuerySchema), guestController.getGuests); // G
 const GuestIdSchema = Type.Object({
   id: Type.String(), // Change to Type.String() for route params
 });
-router.get("/:id", validate(GuestIdSchema), guestController.getGuestById); // GET /guests/:id : requires input
-router.put("/:id", validate(GuestIdSchema), guestController.updateGuest); // PUT /guests/:id : requires input
-router.delete("/:id", validate(GuestIdSchema), guestController.deleteGuest); // DELETE /guests/:id : requires input
+router.get(
+  "/:id",
+  validate(GuestIdSchema, "params"),
+  guestController.getGuestById
+);
+router.put(
+  "/:id",
+  validate(GuestIdSchema, "params"),
+  guestController.updateGuest
+);
+router.delete(
+  "/:id",
+  validate(GuestIdSchema, "params"),
+  guestController.deleteGuest
+);
 
 // schema for input validation using typebox
 const AddGuestSchema = Type.Object({
