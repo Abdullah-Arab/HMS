46a44b25b1ac3f7179fb22b8140ee0cd4a9fcf85
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-08 22:03:19 +0200

    Update navigation links and icons in app component for consistency

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { TuiRoot } from '@taiga-ui/core';
 import { TuiAccordion } from '@taiga-ui/kit';
 import { TuiIcon } from '@taiga-ui/core';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TuiRoot, TuiAccordion, TuiIcon],
@@ -15,22 +14,22 @@ export class AppComponent {
   routes = signal([
     {
       title: 'Dashboard',
-      icon: '@tui.layout-dashboard',
+      icon: 'layout-dashboard',
       link: '/',
     },
     {
       title: 'Rooms',
-      icon: '@tui.bed-single',
+      icon: 'bed-single',
       link: '/rooms',
     },
     {
       title: 'Guests',
-      icon: '@tui.users',
+      icon: 'users',
       link: '/guests',
     },
     {
       title: 'Reservations',
-      icon: '@tui.calendar-fold',
+      icon: 'calendar-fold',
       link: '/reservations',
     },
   ]);
