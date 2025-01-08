bdcad2142ba9a5a0fd0c783906f8db1aaaffe0d8
Abdullah Arab <abdo.arab2014@gmail.com>
2025-01-08 13:04:12 +0200

    Add Taiga UI components and styles to the Angular project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'World';
 }
