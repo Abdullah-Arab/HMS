8f84a154d023f929b71dc53a6be8d5745f4d74e2
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-09 08:43:16 +0200

    Add RoomService and implement RoomsComponent as standalone

--- frontend/src/app/components/rooms/rooms.component.ts
+++ frontend/src/app/components/rooms/rooms.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { RoomService } from './../../services/room.service';
+import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-rooms',
+  standalone: true,
   imports: [],
-  templateUrl: './rooms.component.html'
+  templateUrl: './rooms.component.html',
 })
 export class RoomsComponent {
-
+ 
 }
