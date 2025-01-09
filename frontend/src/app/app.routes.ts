b30d6b547d0329177c64d2ac967b9eafd28eb060
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-09 08:28:56 +0200

    Implement lazy loading for dashboard, rooms, guests, and reservations components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,18 +8,35 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: DashboardComponent,
+    // component: DashboardComponent,
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
   },
   {
     path: 'rooms',
-    component: RoomsComponent,
+    // component: RoomsComponent,
+    loadComponent: () =>
+      import('./components/rooms/rooms.component').then(
+        (m) => m.RoomsComponent
+      ),
   },
   {
     path: 'guests',
-    component: GuestsComponent,
+    // component: GuestsComponent,
+    loadComponent: () =>
+      import('./components/guests/guests.component').then(
+        (m) => m.GuestsComponent
+      ),
   },
   {
     path: 'reservations',
-    component: ReservationsComponent,
+    // component: ReservationsComponent,
+    loadComponent: () =>
+      import('./components/reservations/reservations.component').then(
+        (m) => m.ReservationsComponent
+      ),
   },
+  // { path: '**', component: PageNotFoundComponent }
 ];
