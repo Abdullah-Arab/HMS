94027875a060cf397bad2eb7cb0a84b120165dd6
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-10 21:43:01 +0200

    Add support for ES modules and improve environment variable handling in server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config(); // This loads environment variables from the .env file
+// Convert import.meta.url to __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Serve static files from the Angular app
-const buildPath = path.join(__dirname, 'dist', 'frontend');
+const buildPath = path.join(__dirname, "dist");
 app.use(express.static(buildPath));
 
 // Handle Angular routing (redirect all other routes to index.html)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 // Start the server
-const PORT = process.env['PORT'] || 3456;
+const PORT = process.env.PORT || 3456;
 app.listen(PORT, () => {
   console.log(`Frontend is running on http://localhost:${PORT}`);
 });
