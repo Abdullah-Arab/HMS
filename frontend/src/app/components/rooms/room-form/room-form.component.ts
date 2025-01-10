f31b97052fbfd12bf8c17048a406570928d67c5d
Abdullah-Arab <abdo.arab2014@gmail.com>
2025-01-10 15:53:46 +0200

    Implement RoomFormComponent with validation and submission logic

--- frontend/src/app/components/rooms/room-form/room-form.component.ts
+++ frontend/src/app/components/rooms/room-form/room-form.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleComponent } from '../../title/title.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { TuiButton, TuiDataList } from '@taiga-ui/core';
+import { TuiDataListWrapper } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-room-form',
-  imports: [],
-  templateUrl: './room-form.component.html'
+  imports: [
+    TitleComponent,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiDataList,
+    TuiDataListWrapper,
+    ReactiveFormsModule,
+    TuiButton,
+  ],
+
+  templateUrl: './room-form.component.html',
 })
 export class RoomFormComponent {
+  roomForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.roomForm = this.formBuilder.group({
+      number: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      capacity: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      name: [''],
+    });
+  }
+
+  onSubmit(): void {
+    console.log('hey');
+    if (this.roomForm.valid) {
+      console.log('Form Submitted Successfully!', this.roomForm.value);
+    } else {
+      console.log('Form Submission Failed. Please check the fields.');
+    }
+  }
+
+  get roomNumberContent() {
+    return this.roomForm.get('number')?.value;
+  }
+
+  get roomCapacityContent() {
+    return this.roomForm.get('capacity')?.value;
+  }
 }
